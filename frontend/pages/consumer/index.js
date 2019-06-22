83af69cbf65b0f6c304518a1bf0f30657a001418
Harsh Kedia <harshkedia05@gmail.com>
2019-06-22 16:53:09 +0200

    adding qr code scanner

--- frontend/pages/consumer/index.js
+++ frontend/pages/consumer/index.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import Layout from '../../components/Layout';
-import { Button } from 'semantic-ui-react';
+import { Button, Modal } from 'semantic-ui-react';
 import Styled from 'styled-components';
+import QrReader from 'react-qr-reader';
 
 const ScanButton = Styled(Button)`
   position: absolute;
@@ -10,10 +11,48 @@ const ScanButton = Styled(Button)`
 `;
 
 export default class Consumer extends Component {
+  state = {
+    qrcode: null
+  }
+
+  onUnmount() {
+    console.log('Modal Closed');
+  }
+
+  handleError = err => {
+    console.error(err);
+  }
+
+  handleScan = data => {
+    console.log(data);
+    if (data) {
+      this.setState({
+        qrcode: data
+      })
+    }
+  }
+
   render() {
     return (
-      <Layout>
-        <ScanButton circular secondary size='huge' icon='qrcode'></ScanButton>
+      <Layout id='mount'>
+        <Modal
+          mountNode={document.getElementById('mount')}
+          trigger={<ScanButton circular secondary size='huge' icon='qrcode'></ScanButton>}
+          onUnmount={this.onUnmount}
+          size='fullscreen'
+          basic
+        >
+          <Modal.Content>
+            <Modal.Description>
+              <QrReader
+                delay={300}
+                onError={this.handleError}
+                onScan={this.handleScan}
+                style={{ width: '100%' }}
+              />
+            </Modal.Description>
+          </Modal.Content>
+        </Modal>
       </Layout>
     )
   }
