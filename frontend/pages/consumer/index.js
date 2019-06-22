6c564f2cc21a9d4578ac9f1342b5c70990a19722
Harsh Kedia <harshkedia05@gmail.com>
2019-06-22 19:16:38 +0200

    ui for consumer history

--- frontend/pages/consumer/index.js
+++ frontend/pages/consumer/index.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
 import Layout from '../../components/Layout';
-import { Button, Modal, Header } from 'semantic-ui-react';
+import { Button, Modal, Header, Item, Label } from 'semantic-ui-react';
 import QrReader from 'react-qr-reader';
+import Styled from 'styled-components';
+import Router from 'next/router';
+
+const ScanButton = Styled(Button)`	
+  position: absolute;	
+  right: 10px;	
+  bottom: 10px;	
+`;
+
+const StyledItem = Styled(Item)`
+  &&& {
+    display: flex;
+    background: #FFFFFF;
+    box-shadow: 0 2px 10px 0 rgba(0,0,0,0.07);
+    border-radius: 7px;
+    margin: 7px;
+  }
+`;
+
+const StyledItemContent = Styled(Item.Content)`
+    &&& {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+    }
+`;
 
 export default class Consumer extends Component {
-  state = {
-    qrcode: null
-  }
-
   onUnmount() {
     console.log('Modal Closed');
   }
@@ -19,8 +42,9 @@ export default class Consumer extends Component {
   handleScan = data => {
     console.log(data);
     if (data) {
-      this.setState({
-        qrcode: data
+      Router.push({
+        pathname: '/product',
+        query: data
       })
     }
   }
@@ -46,6 +70,29 @@ export default class Consumer extends Component {
           </Modal.Content>
         </Modal>
         <Header>Scan History</Header>
+        <StyledItem>
+          <Item.Image size='tiny' src='/static/home/tomatoes.png' />
+          <StyledItemContent>
+            <Item.Header as='a'>Tomatoes</Item.Header>
+            <Item.Meta>Kaufland</Item.Meta>
+            <Item.Description>
+              <Label circular color='green'>Vegan</Label>
+              <Label circular color='green'>Bio</Label>
+              <Label>€ 4,5</Label>
+            </Item.Description>
+          </StyledItemContent>
+        </StyledItem>
+        <StyledItem>
+          <Item.Image size='tiny' src='/static/home/sandwich.png' />
+          <StyledItemContent>
+            <Item.Header as='a'>Veggie Sandwich</Item.Header>
+            <Item.Meta>Kaufland</Item.Meta>
+            <Item.Description>
+              <Label circular color='green'>Vegetarian</Label>
+              <Label>€ 2,5</Label>
+            </Item.Description>
+          </StyledItemContent>
+        </StyledItem>
       </Layout>
     )
   }
