1a850616e1767ea68be9f71ac054cc55cc92352c
Felix Hinderer <felix.2.hinderer@uni-kn.de>
2019-06-22 20:18:15 +0200

    product page

--- frontend/pages/product/index.js
+++ frontend/pages/product/index.js
@@ -1,54 +1,110 @@
 import React, { Component } from 'react';
 import Layout from '../../components/Layout';
 import Styled from 'styled-components';
-import { Button } from 'semantic-ui-react';
+import { Button, Image, Header, Divider, Container, Item, Label } from 'semantic-ui-react';
 
-const ScanButton = Styled(Button)`
-  position: absolute;
-  right: 10px;
-  bottom: 10px;
+
+const StyledImage = Styled(Image)`
+  padding-top: 4vh;
 `;
 
-const h2 = Styled.div`
-  display: block;
-  justify-content: center ;
-  text-alignment: center;
-  align-items: top;
-  background: url('/static/login/background.png') center 40% no-repeat;
-  background-size: cover;
-  height: 100vh;
-  width: 100vw !important;
-  padding-top: 10px;
+const StyledIngredientsContainer = Styled(Container)`
+  background: rgba(20, 20, 20, 0.1);
+  margin-left: 0em !important;
+  margin-right: 0em !important;
+  height: 55vh!important;
 `;
 
-const Container = Styled.div`
-  display: block;
-  justify-content: center ;
-  align-items: top;
-  background: url('/static/login/background.png') center 40% no-repeat;
-  background-size: cover;
-  height: 100vh;
-  width: 100vw !important;
-  padding-top: 10px;
+const StyledIngredientContainer = Styled(Container)`
+  background: rgba(255, 255, 255, 0.9);
+  margin: 5%;
+  height: 25%;
+  border-radius: 3.5%;
 `;
 
-const ProductBox = Styled.div`
-  display: grid;
-  margin: 2px;
-  justify-content: center;
-  align-items: center;
-  height: 100px;
-  width: 98vw;
-  border-radius: 5px;
-  background-color: hsla(50, 85%, 85%, 0.6);
+const StyledItem = Styled(Item)`
+  &&& {
+    display: flex;
+    background: #FFFFFF;
+    box-shadow: 0 2px 10px 0 rgba(0,0,0,0.07);
+    border-radius: 7px;
+    margin: 7px;
+  }
+`;
+
+const StyledItemContent = Styled(Item.Content)`
+    &&& {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+    }
+`;
+
+const StyledProductContainer = Styled(Container)`
+  height: 44vh;
+`;
+
+const StyledIngredientHeader = Styled(Header)`
+  margin-left: 5%;
+  padding-top: 5%;
 `;
 
 export default class Product extends Component {
   render() {
     return (
       <Layout title='Product Details'>
-
-      </Layout>
-    );
+        <StyledProductContainer>
+          <StyledImage src='/static/ingredients/sandwich.png' size='small' centered />
+          <Header as='h2' icon textAlign='center'>
+            <Header.Content>Veggie Sandwich
+          <Header.Subheader>Coop</Header.Subheader>
+            </Header.Content>
+            <Label circular color='black'>Vegetarian</Label>
+            <Label circular color='green'>Bio</Label>
+            <Header.Content>2.50 € | 250 gr</Header.Content>
+          </Header>
+        </StyledProductContainer>
+        <Divider fitted />
+        <StyledIngredientsContainer>
+          <StyledIngredientHeader as='h2' disabled>Ingredients</StyledIngredientHeader>
+          <StyledItem>
+            <Item.Image size='tiny' src='/static/ingredients/oat-bread.png' />
+            <StyledItemContent>
+              <Item.Header as='a'>Oat-Bread</Item.Header>
+              <Item.Meta>Kaufland</Item.Meta>
+              <Item.Description>
+                <Label circular color='green'>Vegan</Label>
+                <Label circular color='green'>Bio</Label>
+                <Label>150 gr</Label>
+              </Item.Description>
+            </StyledItemContent>
+          </StyledItem>
+          <StyledItem>
+            <Item.Image size='tiny' src='/static/ingredients/tomatoes.png' />
+            <StyledItemContent>
+              <Item.Header as='a'>Tomatoes</Item.Header>
+              <Item.Meta>Kaufland</Item.Meta>
+              <Item.Description>
+                <Label circular color='green'>Vegan</Label>
+                <Label circular color='green'>Bio</Label>
+                <Label>20 gr</Label>
+              </Item.Description>
+            </StyledItemContent>
+          </StyledItem>
+          <StyledItem>
+            <Item.Image size='tiny' src='/static/ingredients/mozzarella.png' />
+            <StyledItemContent>
+              <Item.Header as='a'>Mozzarella</Item.Header>
+              <Item.Meta>Kaufland</Item.Meta>
+              <Item.Description>
+                <Label circular color='green'>Bio</Label>
+                <Label>80 gr</Label>
+              </Item.Description>
+            </StyledItemContent>
+          </StyledItem>
+        </StyledIngredientsContainer>
+        </Layout>
+        );
   }
 }
